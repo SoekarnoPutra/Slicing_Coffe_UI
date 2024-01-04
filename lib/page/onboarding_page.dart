b338c0749157e8ef363ec8a2b096047046186cc5
SoekarnoPutra <soekarnoputra65@gmail.com>
2024-01-04 18:40:01 +0800

    slicing UI

---
+++ lib/page/onboarding_page.dart
@@ -0,0 +1,73 @@
+import 'package:coffe_shop/second_page.dart';
+import 'package:flutter/material.dart';
+import 'package:coffe_shop/theme.dart';
+
+
+class onboardingPage extends StatelessWidget {
+  const onboardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Image.asset(
+              'assets/images/bg_images.png',
+              fit: BoxFit.cover,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 50),
+                Center(
+                  child: Text(
+                    'Sweet &\nNaise Coffee',
+                    textAlign: TextAlign.center,
+                    style: boldText.copyWith(fontSize: 24),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  'Naise Coffee can change The \natmosphere in the morning',
+                  textAlign: TextAlign.center,
+                  style: regularText.copyWith(fontSize: 12),
+                ),
+                SizedBox(
+                  height: 60,
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width - (2 * 58),
+                  height: 55,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      elevation: 7,
+                      backgroundColor: colorTheme,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100),
+                      ),
+                    ),
+                    onPressed: () {
+                      // Perubahan di sini: Menambahkan perintah Navigator.push
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => secondPage()),
+                      );
+                    },
+                    child: Text(
+                      'ORDER NOW',
+                      style: semiboldText,
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
